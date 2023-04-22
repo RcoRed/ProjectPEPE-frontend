d2404df7e1ef5ad43e8ba48a72816a82fc3e00af
RcoRed <notpurple.red@gmail.com>
2023-04-22 14:53:07 +0200

    merge dalla master

--- src/api.jsx
+++ src/api.jsx
@@ -4,8 +4,5 @@ async function ApiRecipe(){
     console.log("sono dentro");
     let result = await axios.get("http://localhost:8080/api/recipe");
     console.log(result);
-    
-        
-
 }
 export default ApiRecipe
