b9964f5383cc7e2afa81a425a557efbc513ed265
danielePalazzo <palazzo703@gmail.com>
2023-04-28 14:53:43 +0200

    comincia implementazione searchbar

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,11 +1,14 @@
+import { Form } from 'react-router-dom';
 import '/src/components-style/searchbar.css'
 
 function Searchbar() {
     return (
-        <div className="searchbar">
-            <input placeholder="Cerca una ricetta..." id="searchbarInput"></input>
-        </div>
-
+        <Form className="searchbar" method='GET' action='searchrecipe'>
+            <input
+                name='searchbarInput' 
+                placeholder="Cerca una ricetta..." 
+                id="searchbarInput"/>
+        </Form>
     );
 }
 export default Searchbar;
