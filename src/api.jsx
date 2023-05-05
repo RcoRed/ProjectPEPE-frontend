bd487f47b77ef256905825fd4143a06a1fa50f1d
RcoRed <notpurple.red@gmail.com>
2023-05-05 17:19:43 +0200

    api restored

--- src/api.jsx
+++ src/api.jsx
@@ -1,18 +1,17 @@
 import axios from "axios";
 
 //se arriveranno valori undefined allora il loro default sarà null (cioè cio che vogliamo)
-export default async function ApiRecipe({ myDish = null, namePart = null, myNPage = null, myNRecipes = null }) {
+export default async function ApiRecipe({ myDish = null, namePart = null, myDiet = null, myDifficulty = null, myToCook = null }) {
   console.log("attendi risposta per...");
-  console.log("namePart:"+namePart);
-  console.log("myDish:"+myDish);
-  console.log("myDish:"+myDish);
-  console.log("myDish:"+myDish);
+  console.log(namePart);
+  console.log(myDish);
   let result = await axios.get(`http://localhost:8080/api/recipe`, {
     params: {
       dish: myDish,
       recipeName: namePart,
-      nPage: myNPage,
-      nRecipes: myNRecipes,
+      diet: myDiet,
+      difficulty: myDifficulty,
+      toCook: myToCook,
     },
   });
   console.log(result);
