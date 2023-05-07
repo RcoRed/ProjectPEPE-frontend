a6d4d54610a7b17e1e00e17e1c6405019346606c
RcoRed <notpurple.red@gmail.com>
2023-05-07 20:41:25 +0200

    Aggiunge info se un ingrediente è opzionale e numero di ingredienti per ricetta

--- src/components/full-recipe.jsx
+++ src/components/full-recipe.jsx
@@ -3,12 +3,8 @@ import { useLoaderData } from "react-router-dom";
 import "/src/components-style/full-recipe.css";
 
 export async function loader({ request }) {
-  console.log("dentro loader");
   const url = new URL(request.url);
-  console.log(url);
-  console.log("prima di get");
   let myId = url.searchParams.get("od");
-  console.log(myId);
   const recipe = await ApiFullRecipe({ myId });
   return { recipe };
 }
@@ -18,7 +14,7 @@ export default function FullRecipe() {
   console.log(recipe);
   return (
     <section className="full-recipe-section">
-        <div className="recipe-title-div">
+      <div className="recipe-title-div">
         <h1 className="recipe-title-h1">{recipe.name}</h1>
       </div>
       <div className="div-wrap-info">
@@ -27,43 +23,68 @@ export default function FullRecipe() {
         </div>
         <section className="recipe-info">
           <div>
-            <p>Da cucinare? <span className="span-info">{recipe.toCook}</span></p>  
+            <p>
+              Da cucinare? <span className="span-info">{recipe.toCook}</span>
+            </p>
           </div>
           <div>
-            <p>Difficoltà: <span className="span-info">{recipe.difficulty}</span></p>
+            <p>
+              Difficoltà: <span className="span-info">{recipe.difficulty}</span>
+            </p>
           </div>
           <div>
-            <p>Dieta: <span className="span-info">{recipe.diet}</span></p>
+            <p>
+              Dieta: <span className="span-info">{recipe.diet}</span>
+            </p>
           </div>
           <div>
-            <p>Portata: <span className="span-info">{recipe.dish}</span></p>
+            <p>
+              Portata: <span className="span-info">{recipe.dish}</span>
+            </p>
           </div>
           <div>
-            <p>Tempo di preparazione: <span className="span-info">{recipe.totPreparationTime}m</span></p>
+            <p>
+              Tempo di preparazione:{" "}
+              <span className="span-info">{recipe.totPreparationTime}m</span>
+            </p>
           </div>
           <div>
-            <p>Valori nutrizionali: <span className="span-info">{recipe.totNutritionalValue}kcal</span></p>
+            <p>
+              Valori nutrizionali:{" "}
+              <span className="span-info">
+                {recipe.totNutritionalValue}kcal
+              </span>
+            </p>
           </div>
           <div>
-            <p>Costo medio: <span className="span-info">{recipe.totalCost}</span></p>
+            <p>
+              Costo medio: <span className="span-info">{recipe.totalCost}</span>
+            </p>
           </div>
         </section>
       </div>
       <div className="wrapper">
         <div>
-          <h2 className="ingredient-title">Ingredienti</h2>
-        </div>        
-        <div className="ingredients-div">
-          {recipe.ingredients.map((ingredient, index) => <p key={index} className="ingredient">{ingredient}</p>)}
+          <h2 className="ingredient-title">
+            {" "}
+            {recipe.ingredients.length} Ingredienti
+          </h2>
         </div>
-      </div>      
+        <div className="ingredients-div">
+          {recipe.ingredients.map((ingredient, index) => (
+            <p key={index} className="ingredient">
+              {ingredient}
+            </p>
+          ))}
+        </div>
+      </div>
       <section className="section-description">
         <div>
-            <h3 className="preparation-h3">Preparazione:</h3>
+          <h3 className="preparation-h3">Preparazione:</h3>
         </div>
         <div className="div-description">
-            <p className="description">{recipe.description}</p>
-        </div>     
+          <p className="description">{recipe.description}</p>
+        </div>
       </section>
     </section>
   );
